f7141953be4261407a101ccfb05755077ff9a86a
Welton Carvalho <weltoncarvalhoo@gmail.com>
2021-01-23 18:16:09 -0300

    Seguindo e deixando de seguir outros usuários

--- App/Controllers/AppController.php
+++ App/Controllers/AppController.php
@@ -60,6 +60,7 @@ class AppController extends Action {
             
             $usuario = Container::getModel('Usuario');
             $usuario->__set('nome', $pesquisarPor);
+            $usuario->__set('id', $_SESSION['id']);
             $usuarios = $usuario->getAll();
 
         }
@@ -68,6 +69,26 @@ class AppController extends Action {
 
         $this->render('quemSeguir');
 
+    }
+
+    public function acao() {
+        
+        $this->validaAutenticacao();
+        
+        $acao = isset($_GET['acao']) ? $_GET['acao'] : '';
+        $id_usuario_seguindo = isset($_GET['id_usuario']) ? $_GET['id_usuario'] : '';
+
+        $usuario = Container::getModel('Usuario');
+        $usuario->__set('id', $_SESSION['id']);
+
+        if ($acao == 'seguir') {
+            $usuario->seguirUsuario($id_usuario_seguindo);
+
+        } else if ($acao == 'deixar_de_seguir') {
+            $usuario->deixarSeguirUsuario($id_usuario_seguindo);
+        }
+
+        header('Location: /quem_seguir');
     }
 
 }
